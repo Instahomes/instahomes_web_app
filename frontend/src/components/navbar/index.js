3649f29e28673d83ebbec785e55c88a37c1a5763
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-04-29 00:25:21 +0800

    Make navbar responsive

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,16 +1,62 @@
-import { NavbarFrame, NavbarLogo, NavbarSpan } from "./styles";
+import React, { useState, useEffect } from "react";
+import { NavbarFrame, NavbarLogo, NavbarSpan, menuStyles } from "./styles";
 import { OutlineButton } from "../elements";
 import logo from "../../assets/navbar/logo.svg";
+import { slide as Menu } from "react-burger-menu";
+import { withTheme } from "styled-components";
+import { Link } from "react-router-dom";
 
-const Navbar = () => (
-  <NavbarFrame>
-    <NavbarLogo src={logo} />
-    <div>
-      <NavbarSpan>BLOG</NavbarSpan>
-      <NavbarSpan>FEATURED LISTINGS</NavbarSpan>
-      <OutlineButton className="btn-rubik">SIGNUP FOR BETA</OutlineButton>
-    </div>
-  </NavbarFrame>
+const NavbarItems = ({ isMobile, className }) => (
+  <React.Fragment>
+    <NavbarSpan className={className}>BLOG</NavbarSpan>
+    <NavbarSpan className={className}>FEATURED LISTINGS</NavbarSpan>
+    {isMobile ? (
+      <NavbarSpan className={className}>SIGNUP FOR BETA</NavbarSpan>
+    ) : (
+      <OutlineButton className={`btn-rubik ${className}`}>
+        SIGNUP FOR BETA
+      </OutlineButton>
+    )}
+  </React.Fragment>
 );
 
-export default Navbar;
+const Navbar = ({ theme }) => {
+  const finalMenuStyles = menuStyles(theme);
+  const [width, setWidth] = useState(window.innerWidth);
+  const isMediumScreen =
+    width <=
+    parseInt(
+      theme.breakpoints.md.substring(0, theme.breakpoints.md.length - 2)
+    ) +
+      30;
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return (
+    <NavbarFrame>
+      <Link to="/">
+        <NavbarLogo src={logo} />
+      </Link>
+      {isMediumScreen ? (
+        <Menu styles={finalMenuStyles} right>
+          <NavbarItems isMobile />
+        </Menu>
+      ) : (
+        <div>
+          <NavbarItems />
+        </div>
+      )}
+    </NavbarFrame>
+  );
+};
+
+export default withTheme(Navbar);
--- frontend/src/components/navbar/styles.js
+++ frontend/src/components/navbar/styles.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import { useContext } from "react";
+import styled, { ThemeContext } from "styled-components";
 
 export const NavbarFrame = styled.nav`
   display: flex;
@@ -12,9 +13,58 @@ export const NavbarLogo = styled.img`
   height: 40%;
 `;
 
-export const NavbarSpan = styled.span`
+export const NavbarSpan = styled.a`
   color: ${({ theme }) => theme.colors.darkBlue};
   margin: 0 2em;
   font-size: 16px;
   font-weight: 500;
+
+  &.bm-item {
+    display: inline-block;
+    color: ${({ theme }) => theme.colors.mutedBlue};
+    padding: 1em 0;
+  },
 `;
+
+export const menuStyles = (theme) => ({
+  bmBurgerButton: {
+    position: "sticky",
+    width: "20px",
+    height: "15px",
+  },
+  bmBurgerBars: {
+    background: theme.colors.mutedBlue,
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    top: 0,
+    height: "100%",
+  },
+  bmMenu: {
+    background: theme.colors.darkBlue,
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+    top: 0,
+    left: 0,
+  },
+});
