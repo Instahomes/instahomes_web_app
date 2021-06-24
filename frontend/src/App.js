c3e71af7d9ecd21348e7dbe2199c84c01801832a
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-06-24 14:55:59 +0800

    Update prod and maintenance page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Wishlist from "./views/wishlist";
 import Development from "./views/development";
 import Developer from "./views/developer";
 import InquiryForm from "./views/inquiry-form";
+import Maintenance from "./views/maintenance";
 import EmptyPage from "./components/empty-page";
 
 function App() {
@@ -20,44 +21,48 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <ProtectedRoute
-              isAuthenticated={isAuthenticated()}
-              exact
-              path="/wishlist"
-            >
-              <Wishlist />
-            </ProtectedRoute>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-            <Route exact path="/login">
-              <Signup isLogin />
-            </Route>
-            <Route exact path="/search">
-              <Search />
-            </Route>
-            <Route exact path="/listing/:id">
-              <Listing />
-            </Route>
-            <Route exact path="/development/:id">
-              <Development />
-            </Route>
-            <Route exact path="/developer/:id">
-              <Developer />
-            </Route>
-            <Route exact path="/inquire">
-              <InquiryForm />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route>
-              <EmptyPage isEmpty={true} />
-            </Route>
-          </Switch>
-        </Router>
+        {process.env.REACT_APP_MAINTENANCE_MODE == "true" ? (
+          <Maintenance />
+        ) : (
+          <Router>
+            <Switch>
+              <ProtectedRoute
+                isAuthenticated={isAuthenticated()}
+                exact
+                path="/wishlist"
+              >
+                <Wishlist />
+              </ProtectedRoute>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              <Route exact path="/login">
+                <Signup isLogin />
+              </Route>
+              <Route exact path="/search">
+                <Search />
+              </Route>
+              <Route exact path="/listing/:id">
+                <Listing />
+              </Route>
+              <Route exact path="/development/:id">
+                <Development />
+              </Route>
+              <Route exact path="/developer/:id">
+                <Developer />
+              </Route>
+              <Route exact path="/inquire">
+                <InquiryForm />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route>
+                <EmptyPage isEmpty={true} />
+              </Route>
+            </Switch>
+          </Router>
+        )}
       </ThemeProvider>
     </>
   );
