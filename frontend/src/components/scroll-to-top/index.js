474bca02ebbe4e1d52db151df91fefa83ecb118f
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-06-25 18:48:59 +0800

    Update google analytics listener

--- frontend/src/components/scroll-to-top/index.js
+++ frontend/src/components/scroll-to-top/index.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
-import { useLocation, useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 
 const ScrollToTop = ({ children }) => {
   const location = useLocation();
-  const history = useHistory();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
-  history.listen((location) => {
+  // history.listen((location) => {
+  //   ReactGA.set({ page: location.pathname }); // Update the user's current page
+  //   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  // });
+
+  useEffect(() => {
     ReactGA.set({ page: location.pathname }); // Update the user's current page
     ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  });
+  }, [location]);
 
   return children;
 };
