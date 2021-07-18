b85eae79c75015f32e2fdef4d867114561f4e419
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-07-18 21:08:58 +0800

    Integrate fb chat plugin

--- frontend/src/components/layout/index.js
+++ frontend/src/components/layout/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import TopBar from "../top-bar";
 import Footer from "../footer";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const Frame = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
@@ -11,6 +12,11 @@ export default function Layout({ children }) {
   return (
     <React.Fragment>
       <TopBar />
+      <MessengerCustomerChat
+        pageId="100233035447229"
+        // appId="<APP_ID>"
+        // htmlRef="<REF_STRING>"
+      />
       <Frame>{children}</Frame>
       <Footer />
     </React.Fragment>
