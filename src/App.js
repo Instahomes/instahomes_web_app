aa2a7e83b2bd891280d08c3342a2051a08c0282a
Mukul Chugh <mukulchughofficial@gmail.com>
2022-04-08 10:54:03 +0530

    Old State

--- src/App.js
+++ src/App.js
@@ -89,6 +89,7 @@ function App() {
                 <Route exact path="/">
                   <Home />
                 </Route>
+                <Route exact path="/test"></Route>
                 <Route>
                   <EmptyPage isEmpty={true} />
                 </Route>
